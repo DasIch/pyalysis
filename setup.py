0a7c7c14bc2a10aa91cbcae72e0f69220a5c37fd
Daniel Neuhäuser <dasdasich@gmail.com>
2014-02-10 14:14:06 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+"""
+    setup
+    ~~~~~
+
+    :copyright: 2014 by Daniel Neuhäuser
+    :license: BSD, see LICENSE.rst for details
+"""
+from setuptools import setup
+
+
+setup(
+    name='Pythonic',
+    version='0.1.0-dev',
+    url='https://github.com/DasIch/Pythonic',
+
+    author='Daniel Neuhäuser',
+    author_email='ich@danielneuhaeuser.de'
+)
