64f8155ed88b78dd42f9f396d8eae559636d9e56
Daniel Neuhäuser <dasdasich@gmail.com>
2014-02-11 22:30:56 +0100

    Add load_ignore_filter

--- pyalysis/ignore/__init__.py
+++ pyalysis/ignore/__init__.py
@@ -6,3 +6,11 @@
     :copyright: 2014 by Daniel Neuhäuser and Contributors
     :license: BSD, see LICENSE.rst for details
 """
+from pyalysis.ignore.lexer import lex
+from pyalysis.ignore.parser import parse
+from pyalysis.ignore.verifier import verify
+from pyalysis.ignore.compiler import compile
+
+
+def load_ignore_filter(file):
+    return compile(verify(parse(lex(file.read()), file.name)))
--- tests/test_ignore.py
+++ tests/test_ignore.py
@@ -6,8 +6,11 @@
     :copyright: 2014 by Daniel Neuhäuser and Contributors
     :license: BSD, see LICENSE.rst for details
 """
+from io import StringIO
+
 import pytest
 
+from pyalysis.ignore import load_ignore_filter
 from pyalysis.ignore.lexer import lex
 from pyalysis.ignore.tokens import Name, Location
 from pyalysis.ignore.parser import parse
@@ -68,3 +71,13 @@ def test_verify(source, filters, warnings, recwarn):
 ])
 def test_compile(source, warning, allowed):
     assert compile(verify(parse(lex(source))))(warning) == allowed
+
+
+@pytest.mark.parametrize(('source', 'warning', 'allowed'), [
+    (u'print-statement', PrintStatement(u'message', '<test>', 1), False),
+    (u'print-statement', DivStatement(u'message', '<test>', 1), True)
+])
+def test_load_ignore_filer(source, warning, allowed):
+    file = StringIO(source)
+    file.name = '<test>'
+    assert load_ignore_filter(file)(warning) == allowed
